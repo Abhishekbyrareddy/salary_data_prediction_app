d6d56730b49d3042a9919c057de9dbd668c414c1
Abhishekbyrareddy <162465063+Abhishekbyrareddy@users.noreply.github.com>
2024-11-27 20:10:33 +0100

    Update salary_app_py.py

--- salary_app_py.py
+++ salary_app_py.py
@@ -13,7 +13,7 @@ def main():
     st.title("Salary Prediction App")
 
     # Load the pre-trained model
-    model_path = os.path.join(os.path.dirname(__file__), 'C:\Users\abhishek\OneDrive\Desktop\streamlit files\Test 1\salary_model.pkl')
+    model_path = os.path.join(os.path.dirname(__file__), 'salary_model.pkl')
     try:
         model = joblib.load(model_path)
     except Exception as e:
